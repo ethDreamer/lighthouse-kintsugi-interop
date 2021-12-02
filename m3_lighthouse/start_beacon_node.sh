95ebca5ed4730dc30c4f59b42d066ea5aa45ab39
Mark Mackey <ethereumdreamer@gmail.com>
2021-12-02 13:47:57 -0500

    added missing flag

--- m3_lighthouse/start_beacon_node.sh
+++ m3_lighthouse/start_beacon_node.sh
@@ -3,22 +3,23 @@ source ./vars.env
 echo "Staring lighthouse beacon node using execution engine at $EXECUTION_ENDPOINT..."
 
 $LIGHTHOUSE_BINARY \
-	--spec $SPEC \
-	--testnet-dir $TESTNET_DIR \
-	--debug-level $DEBUG_LEVEL \
-	beacon_node \
-	--datadir $DATADIR \
-	--enr-address  ${DISCOVERY_ADDRESS[lighthouse]} \
-	--enr-udp-port ${DISCOVERY_UDP[lighthouse]} \
-	--enr-tcp-port ${DISCOVERY_TCP[lighthouse]} \
-	--disable-enr-auto-update \
-	--port ${DISCOVERY_TCP[lighthouse]} \
-	--discovery-port ${DISCOVERY_UDP[lighthouse]} \
-	--dummy-eth1 \
-	--http \
-	--metrics \
-	--merge \
-	--execution-endpoints $EXECUTION_ENDPOINT \
-	--terminal-total-difficulty-override $TTD_OVERRIDE \
+    --spec $SPEC \
+    --testnet-dir $TESTNET_DIR \
+    --debug-level $DEBUG_LEVEL \
+    beacon_node \
+    --datadir $DATADIR \
+    --enr-address  ${DISCOVERY_ADDRESS[lighthouse]} \
+    --enr-udp-port ${DISCOVERY_UDP[lighthouse]} \
+    --enr-tcp-port ${DISCOVERY_TCP[lighthouse]} \
+    --disable-enr-auto-update \
+    --port ${DISCOVERY_TCP[lighthouse]} \
+    --discovery-port ${DISCOVERY_UDP[lighthouse]} \
+    --dummy-eth1 \
+    --http-allow-sync-stalled \
+    --http \
+    --metrics \
+    --merge \
+    --execution-endpoints $EXECUTION_ENDPOINT \
+    --terminal-total-difficulty-override $TTD_OVERRIDE \
 
 
